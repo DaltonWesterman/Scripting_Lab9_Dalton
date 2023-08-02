747eeea736c801de4102556c6a26e722ed4cf41f
Dalton Westerman <133789471+DaltonWesterman@users.noreply.github.com>
2023-08-02 16:33:38 -0400

    added new code

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -18,7 +18,7 @@ root.title("Pokemon Information")
 # Create the frames
 
 frame_input = ttk.Frame(root)
-frame_input.grid(row=1, column=0, columnspan=2, pady=(20,10))
+frame_input.grid(row=0, column=0, columnspan=2, pady=(20,10))
 
 frame_info = ttk.LabelFrame(root, text = "Info")
 frame_info.grid(row=1, column = 0, padx = (20 , 10), pady= (10 , 20), sticky = N)
@@ -41,6 +41,15 @@ def handle_button_get_info():
           label_height_value = [text] = str(poke_info['Height:']) + 'dm'
           label_weight_value = [text] = str(poke_info['Weight:']) + 'hg'
           types_list = [t['type']['name'].capitalize() for t in poke_info['types']]
+          label_type_value[t['type']]['name'].capitalize() for t in poke_info['types']]
+          label_type_value['text'] = ', '.join(types_list)
+          bar_hp['value'] = poke_info['stats'][0]['base_stat']
+          #
+          #
+          #
+      else:
+          error_msg = f"Unable to fetch information for" {poke_name.caitalize()} from the POKE_API.'
+          messagebox.showinfo(title='Error', message=err_msg, icon='error')
 
 button_get_info = ttk.Button(frame_input, text='Get Info', command= handle_button_get_info)
 button_get_info.grid(row=0, column=2, padx=10, pady=10)
@@ -51,6 +60,16 @@ label_height = ttk.Label(frame_info, text = 'Height:')
 label_height.grid(row=0, column=0, padx=(10,5), pady=(10,5), sticky=E)
 label_height_volume = ttk.Label(frame_info, width=20)
 label_height_volume.grid(row=1, column=1,padx=(0,10),pady=(10,5), sticky=W)
+
+
+# Now populare the info frame with widgets here
+
+label_weight = ttk.Label(frame_info, text = 'Weight:')
+label_weight.grid(row=1, column=0, padx=(10,5), pady=(10,5), sticky=E)
+label_weight_volume = ttk.Label(frame_info, width=20)
+label_weight_volume.grid(row=1, column=1,padx=(0,10),pady=(10,5), sticky=W)
+
+
 
 
 
